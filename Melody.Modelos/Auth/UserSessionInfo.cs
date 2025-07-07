a111d7950a3ce12d549079c8847aafaf3f3a6770
MANUEL-74-coder <manuelcuesaca@gmail.com>
2025-07-06 20:29:56 -0500

    Implementación de controladores para login y registro

---
+++ Melody.Modelos/Auth/UserSessionInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.Auth
+{
+    public class UserSessionInfo
+    {
+        public string UserId { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public List<string> Roles { get; set; } = new();
+        public string Token { get; set; } = "";
+
+        // Helpers para verificar roles
+        public bool IsArtista => Roles.Contains("artista");
+        public bool IsAdmin => Roles.Contains("admin");
+        public bool IsUserFree => Roles.Contains("userfree");
+        public bool IsUserPremium => Roles.Contains("userpremium");
+    }
+}
