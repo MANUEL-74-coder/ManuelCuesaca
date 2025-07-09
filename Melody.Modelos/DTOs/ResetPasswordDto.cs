c0b5c79b1fd4c74d0fa329fb3930f48e3bdb4a67
MANUEL-74-coder <manuelcuesaca@gmail.com>
2025-07-08 20:56:24 -0500

    Agregar los Dtos para recuperar la contraseña

---
+++ Melody.Modelos/DTOs/ResetPasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Melody.Modelos.DTOs
+{
+    public class ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NuevaPassword { get; set; }
+        public string ConfirmarPassword { get; set; }
+    }
+}
